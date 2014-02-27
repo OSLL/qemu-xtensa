ef6b8a0fb1caa0ffdb1923310ff431a874293854
Max Filippov <jcmvbkbc@gmail.com>
2014-02-27 04:26:21 +0400

    WIP: hw/xtensa: add xtfpga SPI controller
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- hw/xtensa/xtfpga.c
+++ hw/xtensa/xtfpga.c
@@ -125,6 +125,76 @@ static Lx60FpgaState *lx60_fpga_init(MemoryRegion *address_space,
     return s;
 }
 
+typedef struct Lx60SpiState {
+    MemoryRegion iomem;
+    uint32_t start;
+    uint32_t data;
+    int64_t start_time;
+} Lx60SpiState;
+
+static void lx60_spi_reset(void *opaque)
+{
+    Lx60SpiState *s = opaque;
+
+    s->start = 0;
+    s->data = 0;
+    s->start_time = 0;
+}
+
+static uint64_t lx60_spi_read(void *opaque, hwaddr addr,
+        unsigned size)
+{
+    Lx60SpiState *s = opaque;
+
+    switch (addr) {
+    case 0x0: /*start*/
+        return s->start;
+
+    case 0x4: /*busy*/
+        return qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) - s->start_time < 100000;
+    }
+    return 0;
+}
+
+static void lx60_spi_write(void *opaque, hwaddr addr,
+        uint64_t val, unsigned size)
+{
+    Lx60SpiState *s = opaque;
+
+    switch (addr) {
+    case 0x0: /*start*/
+        if ((s->start ^ val) & 1) {
+            s->start_time = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+        }
+        s->start = val & 1;
+        break;
+
+    case 0x8: /*data*/
+        s->data = val;
+        break;
+    }
+}
+
+static const MemoryRegionOps lx60_spi_ops = {
+    .read = lx60_spi_read,
+    .write = lx60_spi_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static Lx60SpiState *lx60_spi_init(MemoryRegion *address_space,
+        hwaddr base)
+{
+    Lx60SpiState *s = g_malloc(sizeof(Lx60SpiState));
+
+    memory_region_init_io(&s->iomem, NULL, &lx60_spi_ops, s,
+            "lx60.spi", 0x10000);
+    memory_region_add_subregion(address_space, base, &s->iomem);
+    lx60_spi_reset(s);
+    qemu_register_reset(lx60_spi_reset, s);
+    return s;
+}
+
+
 static void lx60_net_init(MemoryRegion *address_space,
         hwaddr base,
         hwaddr descriptors,
@@ -259,6 +329,7 @@ static void lx_init(const LxBoardDesc *board, MachineState *machine)
                           224 * 1024 * 1024);
     memory_region_add_subregion(system_memory, 0xf0000000, system_io);
     lx60_fpga_init(system_io, 0x0d020000);
+    lx60_spi_init(system_io, 0x0d0a0000);
     if (nd_table[0].used) {
         lx60_net_init(system_io, 0x0d030000, 0x0d030400, 0x0d800000,
                 xtensa_get_extint(env, 1), nd_table);
