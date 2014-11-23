d75df120d540a3692647ec621e69a1683481d5fe
Max Filippov <jcmvbkbc@gmail.com>
2014-11-24 01:11:56 +0300

    target-xtensa: esp8266: implement most of UART
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- hw/xtensa/esp8266.c
+++ hw/xtensa/esp8266.c
@@ -41,39 +41,257 @@
 #include "qemu/error-report.h"
 #include "bootparam.h"
 
+#define DEFINE_BITS(prefix, reg, field, shift, len) \
+    prefix##_##reg##_##field##_SHIFT = shift, \
+    prefix##_##reg##_##field##_LEN = len, \
+    prefix##_##reg##_##field = ((~0U >> (32 - (len))) << (shift))
+
 /* Serial */
+
+enum {
+    ESP8266_UART_FIFO,
+    ESP8266_UART_INT_RAW,
+    ESP8266_UART_INT_ST,
+    ESP8266_UART_INT_ENA,
+    ESP8266_UART_INT_CLR,
+    ESP8266_UART_CLKDIV,
+    ESP8266_UART_AUTOBAUD,
+    ESP8266_UART_STATUS,
+    ESP8266_UART_CONF0,
+    ESP8266_UART_CONF1,
+    ESP8266_UART_LOWPULSE,
+    ESP8266_UART_HIGHPULSE,
+    ESP8266_UART_RXD_CNT,
+    ESP8266_UART_DATE = 0x78 / 4,
+    ESP8266_UART_ID,
+    ESP8266_UART_MAX,
+};
+
+#define ESP8266_UART_BITS(reg, field, shift, len) \
+    DEFINE_BITS(ESP8266_UART, reg, field, shift, len)
+
+enum {
+    ESP8266_UART_BITS(INT, RXFIFO_FULL, 0, 1),
+    ESP8266_UART_BITS(INT, TXFIFO_EMPTY, 1, 1),
+    ESP8266_UART_BITS(INT, RXFIFO_OVF, 4, 1),
+};
+
+enum {
+    ESP8266_UART_BITS(CONF0, LOOPBACK, 14, 1),
+    ESP8266_UART_BITS(CONF0, RXFIFO_RST, 17, 1),
+};
+
+enum {
+    ESP8266_UART_BITS(CONF1, RXFIFO_FULL, 0, 7),
+};
+
+#define ESP8266_UART_GET(s, _reg, _field) \
+    extract32(s->reg[ESP8266_UART_##_reg], \
+              ESP8266_UART_##_reg##_##_field##_SHIFT, \
+              ESP8266_UART_##_reg##_##_field##_LEN)
+
+#define ESP8266_UART_FIFO_SIZE 0x80
+#define ESP8266_UART_FIFO_MASK 0x7f
 
 typedef struct Esp8266SerialState {
     MemoryRegion iomem;
     CharDriverState *chr;
+    qemu_irq irq;
+
+    unsigned rx_first;
+    unsigned rx_last;
+    uint8_t rx[ESP8266_UART_FIFO_SIZE];
+    uint32_t reg[ESP8266_UART_MAX];
 } Esp8266SerialState;
+
+static unsigned esp8266_serial_rx_fifo_size(Esp8266SerialState *s)
+{
+    return (s->rx_last - s->rx_first) & ESP8266_UART_FIFO_MASK;
+}
+
+static int esp8266_serial_can_receive(void *opaque)
+{
+    Esp8266SerialState *s = opaque;
+
+    return esp8266_serial_rx_fifo_size(s) != ESP8266_UART_FIFO_SIZE - 1;
+}
+
+static void esp8266_serial_irq_update(Esp8266SerialState *s)
+{
+    s->reg[ESP8266_UART_INT_ST] |= s->reg[ESP8266_UART_INT_RAW];
+    if (s->reg[ESP8266_UART_INT_ST] & s->reg[ESP8266_UART_INT_ENA]) {
+        qemu_irq_raise(s->irq);
+    } else {
+        qemu_irq_lower(s->irq);
+    }
+}
+
+static void esp8266_serial_rx_irq_update(Esp8266SerialState *s)
+{
+    if (esp8266_serial_rx_fifo_size(s) >= ESP8266_UART_GET(s, CONF1, RXFIFO_FULL)) {
+        s->reg[ESP8266_UART_INT_RAW] |= ESP8266_UART_INT_RXFIFO_FULL;
+    } else {
+        s->reg[ESP8266_UART_INT_RAW] &= ~ESP8266_UART_INT_RXFIFO_FULL;
+    }
+
+    if (!esp8266_serial_can_receive(s)) {
+        s->reg[ESP8266_UART_INT_RAW] |= ESP8266_UART_INT_RXFIFO_OVF;
+    } else {
+        s->reg[ESP8266_UART_INT_RAW] &= ~ESP8266_UART_INT_RXFIFO_OVF;
+    }
+    esp8266_serial_irq_update(s);
+}
 
 static uint64_t esp8266_serial_read(void *opaque, hwaddr addr,
                                     unsigned size)
 {
-    //Esp8266SerialState *s = opaque;
+    Esp8266SerialState *s = opaque;
 
-    switch (addr) {
-    case 0x1c: /*busy*/
+    if ((addr & 3) || size != 4) {
         return 0;
+    }
 
+    switch (addr / 4) {
+    case ESP8266_UART_STATUS:
+        return esp8266_serial_rx_fifo_size(s);
+
+    case ESP8266_UART_FIFO:
+        if (esp8266_serial_rx_fifo_size(s)) {
+            uint8_t r = s->rx[s->rx_first++];
+
+            s->rx_first &= ESP8266_UART_FIFO_MASK;
+            esp8266_serial_rx_irq_update(s);
+            return r;
+        } else {
+            return 0;
+        }
+    case ESP8266_UART_INT_RAW:
+    case ESP8266_UART_INT_ST:
+    case ESP8266_UART_INT_ENA:
+    case ESP8266_UART_INT_CLR:
+    case ESP8266_UART_CLKDIV:
+    case ESP8266_UART_AUTOBAUD:
+    case ESP8266_UART_CONF0:
+    case ESP8266_UART_CONF1:
+    case ESP8266_UART_LOWPULSE:
+    case ESP8266_UART_HIGHPULSE:
+    case ESP8266_UART_RXD_CNT:
+    case ESP8266_UART_DATE:
+    case ESP8266_UART_ID:
+        return s->reg[addr / 4];
+
+    default:
+        qemu_log("%s: unexpected read @0x%"HWADDR_PRIx"\n", __func__, addr);
+        break;
     }
     return 0;
+}
+
+static void esp8266_serial_receive(void *opaque, const uint8_t *buf, int size)
+{
+    Esp8266SerialState *s = opaque;
+    unsigned i;
+
+    for (i = 0; i < size && esp8266_serial_can_receive(s); ++i) {
+        s->rx[s->rx_last++] = buf[i];
+        s->rx_last &= ESP8266_UART_FIFO_MASK;
+    }
+
+    esp8266_serial_rx_irq_update(s);
+}
+
+static void esp8266_serial_ro(Esp8266SerialState *s, hwaddr addr,
+                              uint64_t val, unsigned size)
+{
+}
+
+static void esp8266_serial_tx(Esp8266SerialState *s, hwaddr addr,
+                              uint64_t val, unsigned size)
+{
+    if (ESP8266_UART_GET(s, CONF0, LOOPBACK)) {
+        if (esp8266_serial_can_receive(s)) {
+            uint8_t buf[] = { (uint8_t)val };
+
+            esp8266_serial_receive(s, buf, 1);
+        }
+
+    } else if (s->chr) {
+        uint8_t buf[1] = { val };
+        qemu_chr_fe_write(s->chr, buf, 1);
+        s->reg[ESP8266_UART_INT_RAW] |= ESP8266_UART_INT_TXFIFO_EMPTY;
+        esp8266_serial_irq_update(s);
+    }
+}
+
+static void esp8266_serial_int_ena(Esp8266SerialState *s, hwaddr addr,
+                                   uint64_t val, unsigned size)
+{
+    s->reg[ESP8266_UART_INT_ENA] = val & 0x1ff;
+    esp8266_serial_irq_update(s);
+}
+
+static void esp8266_serial_int_clr(Esp8266SerialState *s, hwaddr addr,
+                                   uint64_t val, unsigned size)
+{
+    s->reg[ESP8266_UART_INT_ST] &= ~val & 0x1ff;
+    esp8266_serial_irq_update(s);
+}
+
+static void esp8266_serial_set_conf0(Esp8266SerialState *s, hwaddr addr,
+                                     uint64_t val, unsigned size)
+{
+    s->reg[ESP8266_UART_CONF0] = val & 0xffffff;
+    if (ESP8266_UART_GET(s, CONF0, RXFIFO_RST)) {
+        s->rx_first = s->rx_last = 0;
+        esp8266_serial_rx_irq_update(s);
+    }
 }
 
 static void esp8266_serial_write(void *opaque, hwaddr addr,
                                  uint64_t val, unsigned size)
 {
     Esp8266SerialState *s = opaque;
+    static void (* const handler[])(Esp8266SerialState *s, hwaddr addr,
+                                    uint64_t val, unsigned size) = {
+        [ESP8266_UART_FIFO] = esp8266_serial_tx,
+        [ESP8266_UART_INT_RAW] = esp8266_serial_ro,
+        [ESP8266_UART_INT_ST] = esp8266_serial_ro,
+        [ESP8266_UART_INT_ENA] = esp8266_serial_int_ena,
+        [ESP8266_UART_INT_CLR] = esp8266_serial_int_clr,
+        [ESP8266_UART_STATUS] = esp8266_serial_ro,
+        [ESP8266_UART_CONF0] = esp8266_serial_set_conf0,
+        [ESP8266_UART_LOWPULSE] = esp8266_serial_ro,
+        [ESP8266_UART_HIGHPULSE] = esp8266_serial_ro,
+        [ESP8266_UART_RXD_CNT] = esp8266_serial_ro,
+    };
 
-    switch (addr) {
-    case 0x0: /*TX char*/
-        if (s->chr) {
-            uint8_t buf[1] = { val };
-            qemu_chr_fe_write(s->chr, buf, 1);
-        }
-        break;
+    if ((addr & 3) || size != 4 || addr / 4 >= ARRAY_SIZE(handler)) {
+        return;
     }
+
+    if (handler[addr / 4]) {
+        handler[addr / 4](s, addr, val, size);
+    } else {
+        s->reg[addr / 4] = val;
+    }
+}
+
+static void esp8266_serial_reset(void *opaque)
+{
+    Esp8266SerialState *s = opaque;
+
+    memset(s->reg, 0, sizeof(s->reg));
+
+    s->reg[ESP8266_UART_CLKDIV] = 0x2b6;
+    s->reg[ESP8266_UART_AUTOBAUD] = 0x1000;
+    s->reg[ESP8266_UART_CONF0] = 0x1c;
+    s->reg[ESP8266_UART_CONF1] = 0x6060;
+    s->reg[ESP8266_UART_LOWPULSE] = 0xfffff;
+    s->reg[ESP8266_UART_HIGHPULSE] = 0xfffff;
+    s->reg[ESP8266_UART_DATE] = 0x62000;
+    s->reg[ESP8266_UART_ID] = 0x500;
+
+    esp8266_serial_irq_update(s);
 }
 
 static const MemoryRegionOps esp8266_serial_ops = {
@@ -89,9 +307,13 @@ static Esp8266SerialState *esp8266_serial_init(MemoryRegion *address_space,
     Esp8266SerialState *s = g_malloc(sizeof(Esp8266SerialState));
 
     s->chr = chr;
+    s->irq = irq;
+    qemu_chr_add_handlers(s->chr, esp8266_serial_can_receive,
+                          esp8266_serial_receive, NULL, s);
     memory_region_init_io(&s->iomem, NULL, &esp8266_serial_ops, s,
                           "esp8266.serial", 0x300);
     memory_region_add_subregion(address_space, base, &s->iomem);
+    qemu_register_reset(esp8266_serial_reset, s);
     return s;
 }
 
