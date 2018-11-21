baf7c797c5fa215e3b17d1ec9b63bbe5e461b667
Max Filippov <jcmvbkbc@gmail.com>
2018-11-21 12:38:32 -0800

    target/xtensa: cp0_latest: stub extra user registers
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- target/xtensa/core-cp0_latest.c
+++ target/xtensa/core-cp0_latest.c
@@ -10,6 +10,72 @@
 #define xtensa_modules xtensa_modules_cp0_latest
 #include "core-cp0_latest/xtensa-modules.inc.c"
 
+enum {
+    WEIGHT_RED = 8,
+    OUTPUT_RATE = 9,
+    LOOK_UP_MAX_INDEX = 10,
+    MSB_OFFSET = 37,
+    DST_RIB_T2_PTR = 49,
+    SRC_RIB_T2_PTR = 50,
+};
+
+static const XtensaOpcodeOps cp0_ops[] = {
+    {
+        .name = "rur.dst_rib_t2_ptr",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){DST_RIB_T2_PTR},
+    }, {
+        .name = "rur.look_up_max_index",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){LOOK_UP_MAX_INDEX},
+    }, {
+        .name = "rur.msb_offset",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){MSB_OFFSET},
+    }, {
+        .name = "rur.output_rate",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){OUTPUT_RATE},
+    }, {
+        .name = "rur.src_rib_t2_ptr",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){SRC_RIB_T2_PTR},
+    }, {
+        .name = "rur.weight_red",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){WEIGHT_RED},
+    }, {
+        .name = "wur.dst_rib_t2_ptr",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){DST_RIB_T2_PTR},
+    }, {
+        .name = "wur.look_up_max_index",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){LOOK_UP_MAX_INDEX},
+    }, {
+        .name = "wur.msb_offset",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){MSB_OFFSET},
+    }, {
+        .name = "wur.output_rate",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){OUTPUT_RATE},
+    }, {
+        .name = "wur.src_rib_t2_ptr",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){SRC_RIB_T2_PTR},
+    }, {
+        .name = "wur.weight_red",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){WEIGHT_RED},
+    }
+};
+
+static const XtensaOpcodeTranslators cp0_opcodes = {
+    .num_opcodes = ARRAY_SIZE(cp0_ops),
+    .opcode = cp0_ops,
+};
+
 static XtensaConfig cp0_latest __attribute__((unused)) = {
     .name = "cp0_latest",
     .gdb_regmap = {
@@ -18,6 +84,11 @@ static XtensaConfig cp0_latest __attribute__((unused)) = {
         }
     },
     .isa_internal = &xtensa_modules,
+    .opcode_translators = (const XtensaOpcodeTranslators *[]){
+        &cp0_opcodes,
+        &xtensa_core_opcodes,
+        NULL,
+    },
     .clock_freq_khz = 600000,
     DEFAULT_SECTIONS
 };
