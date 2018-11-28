68d65d035459d715e8ed5009674ed73356cdcbe3
Max Filippov <jcmvbkbc@gmail.com>
2018-11-27 17:17:43 -0800

    target/xtensa: cp0_latest: stub cp4 registers
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- target/xtensa/core-cp0_latest.c
+++ target/xtensa/core-cp0_latest.c
@@ -14,6 +14,11 @@ enum {
     WEIGHT_RED = 8,
     OUTPUT_RATE = 9,
     LOOK_UP_MAX_INDEX = 10,
+    UR_W = 16,
+    UR_MY = 32,
+    UR_ACCUM = 40,
+    UR_RESULT = 42,
+    UR_SS = 57,
     MSB_OFFSET = 37,
     DST_RIB_T2_PTR = 49,
     SRC_RIB_T2_PTR = 50,
@@ -21,6 +26,16 @@ enum {
 
 static const XtensaOpcodeOps cp0_ops[] = {
     {
+        .name = "rur.accum_0",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_ACCUM + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.accum_1",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_ACCUM + 1},
+        .coprocessor = 0x10,
+    }, {
         .name = "rur.dst_rib_t2_ptr",
         .translate = translate_rur,
         .par = (const uint32_t[]){DST_RIB_T2_PTR},
@@ -33,17 +48,147 @@ static const XtensaOpcodeOps cp0_ops[] = {
         .translate = translate_rur,
         .par = (const uint32_t[]){MSB_OFFSET},
     }, {
+        .name = "rur.mya",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_MY + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.myb",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_MY + 1},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.myc",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_MY + 2},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.myd",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_MY + 3},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.mye",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_MY + 4},
+        .coprocessor = 0x10,
+    }, {
         .name = "rur.output_rate",
         .translate = translate_rur,
         .par = (const uint32_t[]){OUTPUT_RATE},
+    }, {
+        .name = "rur.result_0",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_RESULT + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.result_1",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_RESULT + 1},
+        .coprocessor = 0x10,
     }, {
         .name = "rur.src_rib_t2_ptr",
         .translate = translate_rur,
         .par = (const uint32_t[]){SRC_RIB_T2_PTR},
     }, {
+        .name = "rur.ss",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_SS},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w0",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w1",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 1},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w10",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 10},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w11",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 11},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w12",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 12},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w13",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 13},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w14",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 14},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w15",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 15},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w2",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 2},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w3",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 3},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w4",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 4},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w5",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 5},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w6",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 6},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w7",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 7},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w8",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 8},
+        .coprocessor = 0x10,
+    }, {
+        .name = "rur.w9",
+        .translate = translate_rur,
+        .par = (const uint32_t[]){UR_W + 9},
+        .coprocessor = 0x10,
+    }, {
         .name = "rur.weight_red",
         .translate = translate_rur,
         .par = (const uint32_t[]){WEIGHT_RED},
+    }, {
+        .name = "wur.accum_0",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_ACCUM + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.accum_1",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_ACCUM + 1},
+        .coprocessor = 0x10,
     }, {
         .name = "wur.dst_rib_t2_ptr",
         .translate = translate_wur,
@@ -57,13 +202,133 @@ static const XtensaOpcodeOps cp0_ops[] = {
         .translate = translate_wur,
         .par = (const uint32_t[]){MSB_OFFSET},
     }, {
+        .name = "wur.mya",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_MY + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.myb",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_MY + 1},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.myc",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_MY + 2},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.myd",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_MY + 3},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.mye",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_MY + 4},
+        .coprocessor = 0x10,
+    }, {
         .name = "wur.output_rate",
         .translate = translate_wur,
         .par = (const uint32_t[]){OUTPUT_RATE},
     }, {
+        .name = "wur.result_0",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_RESULT + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.result_1",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_RESULT + 1},
+        .coprocessor = 0x10,
+    }, {
         .name = "wur.src_rib_t2_ptr",
         .translate = translate_wur,
         .par = (const uint32_t[]){SRC_RIB_T2_PTR},
+    }, {
+        .name = "wur.ss",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_SS},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w0",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 0},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w1",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 1},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w10",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 10},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w11",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 11},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w12",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 12},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w13",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 13},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w14",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 14},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w15",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 15},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w2",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 2},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w3",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 3},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w4",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 4},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w5",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 5},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w6",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 6},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w7",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 7},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w8",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 8},
+        .coprocessor = 0x10,
+    }, {
+        .name = "wur.w9",
+        .translate = translate_wur,
+        .par = (const uint32_t[]){UR_W + 9},
+        .coprocessor = 0x10,
     }, {
         .name = "wur.weight_red",
         .translate = translate_wur,
