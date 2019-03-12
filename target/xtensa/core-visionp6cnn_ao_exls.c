5740ab444935ea7056ec5c152cdedc2cf905d4ce
Max Filippov <jcmvbkbc@gmail.com>
2019-03-11 19:05:09 -0700

    target/xtensa: add visionp6cnn_ao_exls core
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ target/xtensa/core-visionp6cnn_ao_exls.c
@@ -0,0 +1,25 @@
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "exec/gdbstub.h"
+#include "qemu-common.h"
+#include "qemu/host-utils.h"
+
+#include "core-visionp6cnn_ao_exls/core-isa.h"
+#include "overlay_tool.h"
+
+#define xtensa_modules xtensa_modules_visionp6cnn_ao_exls
+#include "core-visionp6cnn_ao_exls/xtensa-modules.inc.c"
+
+static XtensaConfig visionp6cnn_ao_exls __attribute__((unused)) = {
+    .name = "visionp6cnn_ao_exls",
+    .gdb_regmap = {
+        .reg = {
+#include "core-visionp6cnn_ao_exls/gdb-config.inc.c"
+        }
+    },
+    .isa_internal = &xtensa_modules,
+    .clock_freq_khz = 10000,
+    DEFAULT_SECTIONS
+};
+
+REGISTER_CORE(visionp6cnn_ao_exls)
