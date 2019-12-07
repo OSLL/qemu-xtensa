06445b2d3244e52351ef6666d558f9c68b61db4c
Max Filippov <jcmvbkbc@gmail.com>
2019-12-06 18:56:00 -0800

    target/xtensa: import sample_controller32 core
    This is sample_controller core from RI.2 with 32 MPU entries.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ target/xtensa/core-sample_controller32.c
@@ -0,0 +1,26 @@
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "exec/gdbstub.h"
+#include "qemu-common.h"
+#include "qemu/host-utils.h"
+
+#include "core-sample_controller32/core-isa.h"
+#include "core-sample_controller32/core-matmap.h"
+#include "overlay_tool.h"
+
+#define xtensa_modules xtensa_modules_sample_controller32
+#include "core-sample_controller32/xtensa-modules.c.inc"
+
+static XtensaConfig sample_controller32 __attribute__((unused)) = {
+    .name = "sample_controller32",
+    .gdb_regmap = {
+        .reg = {
+#include "core-sample_controller32/gdb-config.c.inc"
+        }
+    },
+    .isa_internal = &xtensa_modules,
+    .clock_freq_khz = 40000,
+    DEFAULT_SECTIONS
+};
+
+REGISTER_CORE(sample_controller32)
